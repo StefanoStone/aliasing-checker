bec4c779ca4d5cff55f5e128f059ea66e0009c99
StefanoStone <stefanocamp187@gmail.com>
2023-06-09 19:12:36 +0200

    implements merge of alias tuples

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import jellyfish
 from typing import List
 from pydriller import Repository
+from collections import defaultdict
 
 
 class Contributor:
@@ -62,16 +63,39 @@ def filter_aliases_by_attribute(contributors: List[Contributor], attribute: str)
             if i == j:
                 continue
 
-            if compare_strings(value, data[j]):
+            if is_alias(value, data[j]):
                 if (j, i) not in detected_aliases:
                     detected_aliases.append((i, j))
 
     return detected_aliases
 
 
-def compare_strings(string1, string2):
+def is_alias(string1, string2):
     # TODO change heuristics to something more sophisticated
     return jellyfish.jaro_distance(string1, string2) > 0.70
+
+
+def dfs(adj_list, visited, vertex, result, key):
+    visited.add(vertex)
+    result[key].append(vertex)
+    for neighbor in adj_list[vertex]:
+        if neighbor not in visited:
+            dfs(adj_list, visited, neighbor, result, key)
+
+
+def merge_aliases(edges):
+    adj_list = defaultdict(list)
+    for x, y in edges:
+        adj_list[x].append(y)
+        adj_list[y].append(x)
+
+    result = defaultdict(list)
+    visited = set()
+    for vertex in adj_list:
+        if vertex not in visited:
+            dfs(adj_list, visited, vertex, result, vertex)
+
+    return result.values()
 
 
 def _main(_args):
@@ -87,17 +111,18 @@ def _main(_args):
         print(contributor)
 
     aliases_by_email = filter_aliases_by_attribute(contributors, 'email')
-    print(f'Aliases by email (len = {len(aliases_by_email)}):', aliases_by_email)
     aliases_by_name = filter_aliases_by_attribute(contributors, 'name')
-    print(f'Aliases by name (len = {len(aliases_by_name)}):', aliases_by_name)
     aliases = list(set(aliases_by_email + aliases_by_name))
-    print(f'Aliases (len = {len(aliases)}):', aliases)
+    aliases = sorted(aliases, key=lambda x: x[0])
+    aliases = merge_aliases(aliases)
 
-    for i in range(len(contributors)):
-        for alias in aliases:
-            if i == alias[0]:
-                contributors[i].merge_alias(contributors[alias[1]])
+    index_to_delete = []
+    for alias in aliases:
+        for i in range(1, len(alias)):
+            contributors[alias[0]].merge_alias(contributors[alias[i]])
+            index_to_delete.append(alias[i])
 
+    contributors = [contributors[i] for i in range(len(contributors)) if i not in index_to_delete]
     print(f'\nContributors after filter names (len = {len(contributors)}):')
     for contributor in contributors:
         print(contributor)
