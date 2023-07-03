7836e366cb30c0262d4ec665b55346f271804bb3
StefanoStone <stefanocamp187@gmail.com>
2023-07-03 19:42:52 +0200

    add new tests, push test repos

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import datetime
+
 from main import *
 
 
@@ -62,3 +64,35 @@ def test_extract_name_false():
     expected_name = "test1"
     name = extract_name_from_email(email)
     assert name != expected_name
+
+
+def test_get_contributors():
+    git_repo = Repository('test_repos/complex_repo')
+    commits = list(git_repo.traverse_commits())
+    contributors = get_contributors_set_from_commits(commits)
+    assert len(contributors) == 1 and contributors[0].name == 'Maurício Aniche'\
+           and contributors[0].email == 'mauricioaniche@gmail.com'
+
+
+def test_get_branches():
+    git_repo = Repository('test_repos/complex_repo')
+    commits = list(git_repo.traverse_commits())
+    contributors = get_contributors_set_from_commits(commits)
+    branches = get_working_branches(contributors[0])
+    expected_branches = ['master', 'b2']
+    assert set(branches) == set(expected_branches)
+
+
+def test_get_dates():
+    git_repo = Repository('test_repos/complex_repo')
+    commits = list(git_repo.traverse_commits())
+    contributors = get_contributors_set_from_commits(commits)
+    start, end = get_working_date_range(contributors[0])
+    expected_start = datetime.datetime(2014, 9, 26, 1, 22, 43)
+    expected_end = datetime.datetime(2015, 7, 21, 11, 38, 40)
+    # remove timezone from datetime
+    start = start.replace(tzinfo=None)
+    end = end.replace(tzinfo=None)
+
+    assert start == expected_start and end == expected_end
+
